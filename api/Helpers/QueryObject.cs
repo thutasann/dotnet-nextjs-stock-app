7dcd781a941dd8d02e0b1e4cd940f20a60013321
thutasann <thutasann2002@gmail.com>
2024-01-07 14:41:03 +0800

    feat: pagination

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -6,5 +6,15 @@ namespace api.Helpers
         public string? CompanyName { get; set; } = null;
         public string? StortBy { get; set; } = null;
         public bool IsDecsending { get; set; } = false;
+
+        /// <summary>
+        /// Page Number for Pagination
+        /// </summary>
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Page Size for Pagination
+        /// </summary>
+        public int PageSize { get; set; } = 20;
     }
 }
